22a0a0c084898946efaca783bcbde445021771de
ElevenNotes <github@11notes.ch>
2025-02-27 14:33:45 +0100

    more testing with stdout and easy steps (where does it break)

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ try{
   Eleven.info('starting action-docker-readme');
   readme.init();
   */
-  const db = new Database('/home/runner/.cache/grype/db/5/vulnerability.db', {
-    fileMustExist:true,
-    readonly:true,
-  });
-  const query = db.prepare('SELECT * FROM id WHERE schema_version = 5').all();
+  process.stdout.write(inspect('starting test', {showHidden:false, depth:null, colors:true}) + os.EOL);
+  const db = new Database('/home/runner/.cache/grype/db/5/vulnerability.db');
+  process.stdout.write(inspect('db connected', {showHidden:false, depth:null, colors:true}) + os.EOL);
+  const stmt = db.prepare('SELECT * FROM id WHERE schema_version = 5');
+  process.stdout.write(inspect('statement prepared', {showHidden:false, depth:null, colors:true}) + os.EOL);
+  const query = stmt.all();
   process.stdout.write(inspect(query, {showHidden:false, depth:null, colors:true}) + os.EOL);
 }catch(e){
   process.stderr.write(inspect(e, {showHidden:false, depth:null}) + os.EOL);
