2b2589d96e2f53f8583b27e0d9c3629d0e650b87
ElevenNotes <github@11notes.ch>
2025-06-23 14:35:18 +0200

    [fix] compose version update

--- dist/index.js
+++ dist/index.js
@@ -27373,6 +27373,7 @@ module.exports = class README{
     const yaml = YAML.parse(compose);
     for(const service in yaml.services){
       if(new RegExp(this.#json.image, 'i').test(yaml.services[service].image)){
+        Eleven.info(`#compose :: found ${yaml.services[service].image} in service ${service}, updating with latest version`)
         compose = compose.replace(new RegExp(yaml.services[service].image, 'ig'), `${this.#json.image}:${this.#json.semver.version}`);
       }
     }
